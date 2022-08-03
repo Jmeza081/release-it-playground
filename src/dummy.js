9fc94797dd36ec7dbb3b81dbc086cbdeb77a7c07
Jesse Meza <jmeza@spoton.com>
2022-08-03 14:38:07 -0500

    Add a new file

---
+++ src/dummy.js
@@ -0,0 +1,7 @@
+/**
+ * This is just a dummy file I am modifying to check if the release-it feature works
+ * as expected!
+ * 
+ * 12323 Numbers! Strings! Remove me!
+ */
+console.log('Hello world!');
