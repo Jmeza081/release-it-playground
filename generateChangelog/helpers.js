c9db98dfba0b26f795ad5542bd666812f4f6cc21
dmakhov <dmakhov-transcenda@spoton.com>
2022-12-14 14:14:36 +0200

    CWP-1704. Trying to generate message.

--- generateChangelog/helpers.js
+++ generateChangelog/helpers.js
@@ -3,5 +3,18 @@ module.exports = function (Handlebars) {
         return releases.sort(function(a, b) {
             return new Date(b.isoDate) - new Date(a.isoDate);
         });
-    })
+    });
+
+    Handlebars.registerHelper('formatMessage', function (options) {
+        const message = options.fn(this);
+
+        const jiraTicketPart = message.match(/\[([CWP])\w+-[0-9]+\]/gi) || '';
+        console.log('jiraTicketPart', jiraTicketPart);
+
+        if(!jiraTicketPart) {
+            return message; // No point in parsing if we aren't following conventions
+        }
+
+        return message;
+    });
 }
