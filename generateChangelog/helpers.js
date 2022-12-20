5a2d185509a82a160c0353f606b2d358c63270f3
dmakhov <dmakhov-transcenda@spoton.com>
2022-12-20 13:54:37 +0200

    CWP-1704. Removed: again removed comment.

--- generateChangelog/helpers.js
+++ generateChangelog/helpers.js
@@ -1,4 +1,4 @@
-module.exports = function (Handlebars) {// added cool comment (changed)
+module.exports = function (Handlebars) {// added cool commen
     Handlebars.registerHelper('sortReleasesByDate', function (releases) {
         return releases.sort(function(a, b) {
             return new Date(b.isoDate) - new Date(a.isoDate);
