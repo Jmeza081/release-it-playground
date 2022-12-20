23a76208dc1c751d2880b2ece3fbe2fd46c06f9f
dmakhov <dmakhov-transcenda@spoton.com>
2022-12-20 13:50:58 +0200

    Added: CWP-1704. Added comment 1.

--- generateChangelog/helpers.js
+++ generateChangelog/helpers.js
@@ -1,4 +1,4 @@
-module.exports = function (Handlebars) {
+module.exports = function (Handlebars) {// added comment
     Handlebars.registerHelper('sortReleasesByDate', function (releases) {
         return releases.sort(function(a, b) {
             return new Date(b.isoDate) - new Date(a.isoDate);
